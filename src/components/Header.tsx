f11e6a08d5a4ebe7ae2f4d44714487003549f2df
Kosei Oki <koseioki1225@gmail.com>
2026-04-24 13:10:37 +0200

    added data

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      Accessible Web Components
+    </header>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+
+       <p>These components were created for developers at <a href="https://www.bankdata.dk" className="plain-link">Bankdata</a> to help remediate the components used on the websites or netbanks of its customer local banks, such as <a href="https://www.jyskebank.dk" className="plain-link">Jyske Bank</a>, <a href="https://www.sydbank.dk" className="plain-link">Sydbank</a>, <a href="https://www.landbobanken.dk" className="plain-link">Landbobank</a>, <a href="https://www.spks.dk" className="plain-link">Sparekassen Sjælland-Fyn</a>, <a href="https://www.skjernbank.dk" className="plain-link">Skjern Bank</a>, and <a href="https://www.djurslandsbank.dk" className="plain-link">Djurslands Bank</a>.</p>
+      <p>Each component includes a working demo and comprehensive specification documents for developers.</p>
+     
+    </div>
+  );
+}
